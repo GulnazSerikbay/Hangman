8ac2682eeac531f9f6e9c5fdcebb324558898415
Gulnaz Serikbay <gserikba@andrew.cmu.edu>
2020-07-22 14:59:46 +0600

    put hangman code into server file
    but now working properly

--- echo_client.py
+++ echo_client.py
@@ -8,7 +8,7 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('localhost', 9056))
 
 
-s.sendall(input("enter your word").encode())
+s.sendall(input("enter your word... ").encode())
 
 data = s.recv(1024)
 
--- echo_server_9056.py
+++ echo_server_9056.py
@@ -1,6 +1,5 @@
 import socket # Use the python socket library
-
-
+import random
 
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -26,11 +25,158 @@ while True:
     # Print the received data
     print('Got this string: ' + word)
 
+    result = 0
 
+    def display_hangman(lives):
+        stages = [  # final state: head, torso, both arms, and both legs
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |     \|/
+                    |      |
+                    |     / \
+                    -
+                    """,
+                    # head, torso, both arms, and one leg
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |     \|/
+                    |      |
+                    |     /
+                    -
+                    """,
+                    # head, torso, and both arms
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |     \|/
+                    |      |
+                    |
+                    -
+                    """,
+                    # head, torso, and one arm
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |     \|
+                    |      |
+                    |
+                    -
+                    """,
+                    # head and torso
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |      |
+                    |      |
+                    |
+                    -
+                    """,
+                    # head
+                    """
+                    --------
+                    |      |
+                    |      O
+                    |
+                    |
+                    |
+                    -
+                    """,
+                    # initial empty state
+                    """
+                    --------
+                    |      |
+                    |
+                    |
+                    |
+                    |
+                    -
+                    """
+        ]
+        return stages[lives]
+
+
+    def play(word):
+
+        lives = 5
+        word_completion = "_" * len(word)
+        guessed = False
+        guessed_letters = [] #what letters have been guessed
+        guessed_words = []
+        print("Hey let's get started! Can you guess the word?'")
+        print("You have " + str(lives) + " lives.")
+        print(word_completion + "\n")
+
+        while not guessed and lives > 0:
+            guess = input("Try to guess the hidden word: ").upper() #receiving the use input "guess"
+
+            if len(guess) == 1 and guess.isalpha():
+                if guess in guessed_letters:
+                    print("You have already guessed the letter, man..." + guess)
+
+                elif guess not in word:
+                    print("Nope, " + guess + " is not in the word.\n (-_-)")
+                    lives = lives - 1
+                    print("You have " + str(lives) + " lives.")
+                    guessed_letters.append(guess)
+
+                else:
+                    print("Well done! " + guess + " is in the word. Keep going!")
+                    guessed_letters.append(guess)
+                    word_as_list = list(word_completion)
+                    indices = [i for i, letter in enumerate(word) if letter == guess]
+                    for index in indices:
+                        word_as_list[index] = guess
+                    word_completion = "".join(word_as_list)
+                    if "_" not in word_completion:
+                        guessed = True
+
+            elif len(guess) == len(word) and guess.isalpha():
+
+                if guess in guessed_words:
+                    print("You've already guessed this word!" + guess)
+                elif guess != word:
+                    print(guess + "is not the actual word. (*_*)")
+                    lives = lives - 1
+                    print("You have " + str(lives) + " lives.")
+                    guesses_words.append(guess)
+                else:
+                    guessed = True
+                    word_completion = word
+            else:
+                print("Please, enter a valid answer")
+            print(display_hangman(lives))
+            print(word_completion + "\n")
+
+        if guessed:
+            print("YAAAY! You've guessed the word! Winner!")
+            result = 1 + result
+
+        else:
+            print("OOH! You lost! Sorry, you will win next time! The word was " + word)
+
+
+    def main():
+        return word.upper()
+        play(word)
+
+        while input("\nPlay Again? (Y/N) \n").upper() == "Y":
+           play(word)
+
+        else: s2.sendall(result)
+
+    if __name__ == "__main__":
+        main()
 
 
     # Send back the encoded string to a socket
-    s2.sendall(data.encode())
+    s2.sendall(word.encode())
 
     # Close the socket's connection
     s2.close()
